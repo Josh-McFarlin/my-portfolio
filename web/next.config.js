74d9db5a4d4e71e7dc6b9a80d1b22379dd4b76f2
Josh McFarlin <32019743+Josh-McFarlin@users.noreply.github.com>
2023-07-06 16:49:32 -0400

    Update next.config.js

--- web/next.config.js
+++ web/next.config.js
@@ -1,34 +1,9 @@
-const withOffline = require("next-offline");
+const { withPlausibleProxy } = require('next-plausible')
 
-const isProd = process.env.NODE_ENV === "production";
-
-const config = {
-  // swcMinify: true,
+module.exports = withPlausibleProxy()({
   generateBuildId: () => "build",
   images: {
     domains: ["cdn.sanity.io"],
     formats: ["image/avif", "image/webp"],
   },
-  // experimental: {
-  //   concurrentFeatures: true,
-  //   serverComponents: true,
-  // },
-  // scope: "/",
-  // workboxOpts: {
-  //   swDest: "service-worker.js",
-  //   runtimeCaching: [
-  //     {
-  //       urlPattern: /^https?.*/,
-  //       handler: "NetworkFirst",
-  //       options: {
-  //         cacheName: "offlineCache",
-  //         expiration: {
-  //           maxEntries: 200,
-  //         },
-  //       },
-  //     },
-  //   ],
-  // },
-};
-
-module.exports = isProd ? withOffline(config) : config;
+});
